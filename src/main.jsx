a08a62d40ec331676628bca3ca5e4d21ca54339b
Kunal <kinholkark380@gmail.com>
2024-05-20 12:55:55 +0530

    changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Hello() {
-  return <p>Hey my name is KUNAL!</p>;
+  return <p>Hey my name is KUNAL!!!!!!!</p>;
 }
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-      <Hello /> 
-  </React.StrictMode>
+  <>
+    <Hello />
+  </>
 );
